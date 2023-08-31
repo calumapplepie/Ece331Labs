208a8290f111d71d7e395c2046f9cd81be48cb9d
Calum McConnell: Analog Lab <mcconncm@lafayette.edu>
2023-08-31 13:41:24 -0400

    final example

--- Lab01/numberone.m
+++ Lab01/numberone.m
@@ -1,14 +1,8 @@
 clear all
 clf
-H2 = [ones(1,21) zeros(1,87) ones(1,20)];
-h2 = abs(ifft(H2));
-k2 = 0:127;
-H3 = [ones(1,41) zeros(1,175) ones(1,40)];
-h3 = abs(ifft(H3));
-k3 = 0:255;
-figure(1)
-plot(H2)
-figure(2)
-plot(k2,h2)
-figure(3)
-plot(k3,h3);
+x=[1,2,-1,5,6];
+B=[1,0,0,0,1
+    0,1,1,1,2
+    3,2,2,1,5
+    0,0,1,-1,1
+    1,0,2,0,2];
