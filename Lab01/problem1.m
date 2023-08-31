7d9d6a201f629c940ad50dc35d41ac994f7bf815
Calum McConnell: Analog Lab <mcconncm@lafayette.edu>
2023-08-31 14:01:32 -0400

    do problem 1

---
+++ Lab01/problem1.m
@@ -0,0 +1,12 @@
+for i=0:201
+  x(i+1)=cos(3*pi*i/20);
+  if(i>=50 & i <= 90 & ~(i>= 70 & i<= 75))
+    x(i+1) = x(i+1) + 3;
+  end
+  if(i>=20 & i<= 40) 
+      x(i+1) = x(i+1) + exp(-i/50);
+  end
+end
+xlabel('time')
+ylabel('amplitude')
+plot(x)
