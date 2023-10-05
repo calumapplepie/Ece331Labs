70c072821016b943e17db3ebb62dd9bd648fa994
Calum McConnell: Analog Lab <mcconncm@lafayette.edu>
2023-10-05 15:08:09 -0400

    do problem 4

--- Lab06/problem3.m
+++ Lab06/problem3.m
@@ -1,27 +1,59 @@
 clear
 t=0:0.01:15;
-x1=0.5;
-x2=1/3;
-x3=0;
-x4=0.504;
+x10=0.5;
+x20=1/3;
+x30=0;
+x40=0.504;
+x50=(2/pi)^2
 
+x1 = x10;
+x2 = x20;
+x3 = x30;
+x4 = x40;
+x5 = x50;
 
-for k=1:60
-    if rem(k,2)==1;
-        x1=x1+(-1)^((k-1)/2)*2/(pi*k)*cos(k*t)
+p1 = x10^2;
+p2 = x20^2;
+p3 = x30^2;
+p4 = x40^2;
+p5 = x50^2;
+
+x1r = zeros(1,length(t));
+for i=1:length(t);
+    if (mod((t(i) + pi /2), 2*pi) < pi)
+         x1r(i) = 1;
     end
-    x2=x2+(4/pi^2)*((-1)^k/(k)^2)*(cos(k*pi*t))
-    x3=x3+(2/pi)*1/k*sin(pi*k*t);
-    x4=x4+0.504*2/sqrt(1+16*k^2)*cos(2*k*t-atan(4*k));
-    subplot(4,1,1), plot(t,x1)
-    subplot(4,1,2), plot(t,x2)
-    subplot(4,1,3), plot(t,x3)
-    subplot(4,1,4), plot(t,x4)
-    pause
 end
 
+for k=1:60
+    x1c(k) = (-1)^((k-1)/2)*2/(pi*k);
+    x2c(k) = (4/pi^2)*((-1)^k/(k)^2);
+    x3c(k) = (2/pi)*1/k;
+    x4c(k) = 0.504*2/sqrt(1+16*k^2);
+    x5c(k) = 2/pi*1/(1-4*k^2);
 
+    if rem(k,2)==1;
+        x1 = x1+x1c(k)*cos(k*t);
+    end
+    x2 = x2+x2c(k)*(cos(k*pi*t));
+    x3 = x3+x3c(k)*sin(pi*k*t);
+    x4 = x4+x4c(k)*cos(2*k*t-atan(4*k));
+    x5 = x5+ x5c(k)*(exp(j*2*k*t)+exp(-j*2*k*t));
 
+    p1 = p1+1/2 * (x1c(k))^2;
+    p2 = p2+1/2 * (x2c(k))^2;
+    p3 = p3+1/2 * (x3c(k))^2;
+    p4 = p4+1/2 * (x4c(k))^2;
+    p5 = p5+1/2 * (x5c(k))^2;
+    
+    figure(1)
+    subplot(5,1,1), plot(t,x1)
+    subplot(5,1,2), plot(t,x2)
+    subplot(5,1,3), plot(t,x3)
+    subplot(5,1,4), plot(t,x4)
+    subplot(5,1,5), plot(t,x5)
 
-
-
+    MSE(k)=(x1r-x1)*(x1r-x1)';
+end
+figure(2);
+plot(MSE)
