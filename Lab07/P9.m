2480eaff2b3f418e67243a17dedf606e0a529fb4
Sam Satalof <satalofs@lafayette.edu>
2023-10-12 14:39:06 -0400

    fix problems in p9

--- Lab07/P9.m
+++ Lab07/P9.m
@@ -5,10 +5,10 @@ k=1; yi(1) =0;
 t =-10:0.01:10
 for i=t; 
 	if(i>-2&&i<2) y1(k)=1;
-	else y(k)=0;
+	else y1(k)=0;
 	end
 	if(i>-1&&i<1) y2(k)=1;
-	else y(k)=0;
+	else y2(k)=0;
 	end
 	k = k+1;
 end
@@ -17,17 +17,18 @@ step= 1/0.01;
 fstep = step/length(t)
 f     = -step/2    :fstep:step/2-fstep;
 fconv = -step+fstep:fstep:step  -fstep;
+fconv2= -step+fstep:2*fstep:step  -fstep;
 
 figure(1)
 subplot(3,1,1), plot(t,y1);
 subplot(3,1,2), plot(t,y2);
-subplot(3,1,3), plot(t,yconv);
+subplot(3,1,3), plot(yconv);
 figure(2)
 subplot(3,1,1), plot(f,fftshift(abs(fft(y1))));
 subplot(3,1,2), plot(f,fftshift(abs(fft(y2))));
 subplot(3,1,2), plot(fconv,fftshift(abs(fft(yconv))));
 figure(3)
 subplot(2,1,1), plot(fconv,fftshift(abs(fft(yconv))));
-subplot(2,1,2), plot(fconv,fftshift(abs(fft(y1))) .* fftshift(abs(fft(y2))));
+subplot(2,1,2), plot(fconv2,fftshift(abs(fft(y1))) .* fftshift(abs(fft(y2))));
 
 
