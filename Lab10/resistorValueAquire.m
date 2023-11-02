e65f74deaed7a8a2367eba6f29a069d0e25f61a1
Calum McConnell: Analog Lab <mcconncm@lafayette.edu>
2023-11-01 23:29:45 -0400

    add some files

--- Lab10/resistorValueAquire.m
+++ Lab10/resistorValueAquire.m
@@ -3,6 +3,8 @@ wc=2*pi*1500;
 %%%%%%%%% first order circuit
 C=1e-9;
 R1=1/(C*wc) % needed R1 value
+R2= 1000;
+R3= 1000;
 %%%%%%%%%%%%%%%second order circuit 2 Sallen Key circuit
 wc=2*pi*1500;
 C21=1*1e-9; %C2 for first circuit
@@ -21,10 +23,12 @@ R=roots([1,-c,d]);
 R12=R(1) % R1 for second Sallen Key circuit
 R22=R(2) % R2 for second Sallen Key circuit
 %%%%%%%%%%%%%%%%%%%%%%%%%filter simulation similar to pspice
-% this is equations 1 and 2 (repeated twice). All multiplied 
-together
+% this is equations 1 and 2 (repeated twice). All multiplied together
 w=0:0.01:2*pi*3000;
 H1=1./(1+C*R1*1j*w);
 H2=1./((1j*w).^2*C11*C21*R11*R21+1j*w*(C21*(R21+R11))+1);
 H3=1./((1j*w).^2*C12*C22*R12*R22+1j*w*(C22*(R22+R12))+1);
+figure(1)
 plot(w/(2*pi),abs(H1.*H2.*H3))
+figure(2)
+plot(w/(2*pi),abs(H1))
