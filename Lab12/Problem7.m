8c302e63d24eb58872bf74846f17157bd602a1ad
Calum McConnell: Analog Lab <mcconncm@lafayette.edu>
2023-12-07 16:01:28 -0500

    make report pt 1

--- Lab12/Problem7.m
+++ Lab12/Problem7.m
@@ -9,11 +9,11 @@ c = 2^(n+1)*st1+exp(n-1)*st0;
 d = (2^n*cos(pi/3*n))*st1;
 g = (-1)^n*n*st0;
 
-ztrans(a); ztrans(b); ztrans(c); ztrans(d);
+[[a, ztrans(a)]; [c, ztrans(c)]; [d,ztrans(d)]; [g,ztrans(g)]]
 
-a = (z*(z-4))/(z^2-5z+6);
-b = (z-4)/(z^2-5z+6);
+a = (z*(z-4))/(z^2-5*z+6);
+b = (z-4)/(z^2-5*z+6);
 c = (exp(-2)-2)*z/((z-exp(-2))*(z-2));
 d = (z-1)^2/z^3;
 
-iztrans(a); iztrans(b); iztrans(c); iztrans(d);
+[[a,iztrans(a)];[b, iztrans(b)]; [c,iztrans(c)];[d, iztrans(d)]]
