eef575774ef8594189af4d83dbd6b46fffe23754
Calum McConnell: Analog Lab <mcconncm@lafayette.edu>
2023-12-07 16:29:30 -0500

    finish p2

--- Lab12/Problem2.m
+++ Lab12/Problem2.m
@@ -1,11 +1,31 @@
-h(0)=1
-h(1)=2 % this is h(0) =0 (check, replace y by h and x by delta)
-h(2)=4(1)+0.5  % h(1)=1 (check)
-for n=1:1000
-    h(n+2)=-h(n): 
+h1(1)=0;
+h1(2)=1;
+
+h2(1)=1;
+h2(2)=1;
+
+h3(1)=0;
+h3(2)=0;
+
+nmax = 1000;
+for n=1:nmax
+    h1(n+2) = -h(n);
+    h2(n+2) = h2(n+1)-0.25*h2(n);
+    h3(n+2) = h3(n+1)/6+h3(n)/6+(n==1)/3;
 end
-hc(1)=0; 
-n=1:16;
-hc=[hc(1),-0.5*cos(pi*n/2)+sin(pi*n/2)];
-n=0:16;
-plot(n,hc,n,h)
+hc1(1)=0; 
+hc3(1)=-2+4/5+6/5;
+n=1:nmax+1;
+hc1=[hc1(1),-0.5*cos(pi*n/2)+sin(pi*n/2)];
+hc3=[hc3(1),4/5*(1/2).^n+6/5*(-1/3).^n ];
+n=0:nmax+1;
+hc2 = [(1.+n).*(1/2).^n];
+
+figure(1);
+plot(n,hc1,n,h1)
+
+figure(2);
+plot(n,hc2,n,h2)
+
+figure(3);
+plot(n,hc2,n,h2)
